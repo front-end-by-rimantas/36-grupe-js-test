8225904c4089ee8b95b8bb493d27531ae3b408de
Rimantas <belauzas@yahoo.com>
2022-03-15 20:14:50 +0200

    Text

--- math.js
+++ math.js
@@ -1,14 +1,14 @@
 const math = {
-    sudeti: (a, b) => a + b,
-    atimti: (a, b) => a - b,
-    padalinti: (a, b) => a / b,
-    sudauginti: (a, b) => a * b,
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '/': (a, b) => a / b,
+    '*': (a, b) => a * b,
 }
 
-const action = 'padalinti';
+const action = '-';
 const n1 = 7;
 const n2 = 5;
 
-const ans = 0;  // 1.4
+const ans = math[action](n1, n2);
 
-console.log(ans);
+console.log(`${n1} ${action} ${n2} = ${ans}`);
---
+++ text.js
@@ -0,0 +1,9 @@
+const text = {};
+
+const position = 'middle'; // first | middle | last
+
+const word = 'Labas';
+
+const letter = 'x';
+
+console.log(`${position} letter of word "${word}" is ${letter}`);
