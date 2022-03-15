ff6dbdc4a97b6a95af1b5d3c3b7265e38ed92548
Rimantas <belauzas@yahoo.com>
2022-03-15 19:32:34 +0200

    Exer 2

---
+++ math.js
@@ -0,0 +1,14 @@
+const math = {
+    sudeti: (a, b) => a + b,
+    atimti: (a, b) => a - b,
+    padalinti: (a, b) => a / b,
+    sudauginti: (a, b) => a * b,
+}
+
+const action = 'padalinti';
+const n1 = 7;
+const n2 = 5;
+
+const ans = 0;  // 1.4
+
+console.log(ans);
