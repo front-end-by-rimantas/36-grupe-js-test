713f2847d2e612dccc1bdbaf11e266804525fadd
Rimantas <belauzas@yahoo.com>
2022-03-15 20:38:46 +0200

    Text solution

--- text.js
+++ text.js
@@ -1,9 +1,13 @@
-const text = {};
+const text = {
+    first: s => s[0],
+    middle: s => s[Math.floor((s.length - 1) / 2)],
+    last: s => s[s.length - 1],
+};
 
 const position = 'middle'; // first | middle | last
 
-const word = 'Labas';
+const word = 'abcde';
 
-const letter = 'x';
+const letter = text[position](word);
 
 console.log(`${position} letter of word "${word}" is ${letter}`);
